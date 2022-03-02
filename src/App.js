29161daa62f227a6eaf6a7c262c36fa22df276dd
tenenger <tenenger@daum.net>
2022-03-02 19:11:08 +0900

    scss styling study

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import Button from "./components/Button";
 
 function App() {
+  // scss 사용
+  const onClick = (e) => {
+    console.log(e.target);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* scss 사용 */}
+      <div className="buttons">
+        <Button size="large" color="pink" onClick={onClick}>
+          버튼
+        </Button>
+        <Button>버튼</Button>
+        <Button size="small" color="gray">
+          버튼
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" color="pink">
+          버튼
+        </Button>
+        <Button>버튼</Button>
+        <Button size="small" color="gray">
+          버튼
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" color="pink">
+          버튼
+        </Button>
+        <Button>버튼</Button>
+        <Button size="small" color="gray">
+          버튼
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" color="blue" outline>
+          BUTTON
+        </Button>
+        <Button color="gray" outline>
+          BUTTON
+        </Button>
+        <Button size="small" color="pink" outline>
+          BUTTON
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" fullWidth>
+          BUTTON
+        </Button>
+        <Button size="large" fullWidth color="gray">
+          BUTTON
+        </Button>
+        <Button size="large" fullWidth color="pink">
+          BUTTON
+        </Button>
+      </div>
     </div>
   );
 }
